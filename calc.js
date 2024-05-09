e6aaac5393a5ef67631308438a4107af1eb0cf46
Vithustennysan <132119746+Vithustennysan@users.noreply.github.com>
2024-05-09 12:19:02 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,26 @@
+let content = "";
+let flag = 0;
+
+function display(id) {
+    if (flag===1){
+        content = "";
+        flag = 0;
+    }
+  content += document.getElementById(id).innerHTML;
+  document.getElementById("values").innerHTML = content;
+}
+
+function reset() {
+  content = "";
+  document.getElementById("values").innerHTML = "0";
+  document.getElementById("result").innerHTML = "_";
+  document.getElementById("output").innerHTML = "_";
+}
+
+function getResult() {
+  flag = 1;
+  let strExpression = document.getElementById("values").innerHTML;
+  let expression = eval(strExpression);
+  document.getElementById("result").innerHTML = expression;
+  document.getElementById("output").innerHTML = " " + expression;
+}
